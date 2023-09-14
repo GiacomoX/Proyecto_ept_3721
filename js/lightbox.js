0e02bd19242fd06939ec327866fb46534b3f6e38
Giacomo Octavio <122325952+GiacomoX@users.noreply.github.com>
2023-09-13 23:56:48 -0500

    Add files via upload

---
+++ js/lightbox.js
@@ -0,0 +1,25 @@
+
+
+
+function suma(x, y) {
+    suma(2, 3);
+    return x + y;
+ }
+ 
+ function resta(x, y) {
+    resta(3, 2);
+    return x - y;
+ }
+ 
+ function divide(x, y) {
+    divide(16, 8);
+    return x / y;
+ }
+ 
+ function multiplica(x, y) {
+    multiplica(2, 3);
+    return x * y;
+ }
+
+
+console.log(suma, resta, divide, multiplica)
